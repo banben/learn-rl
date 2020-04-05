dfe20d1ff395bf4fa911dc79f000dd14898535df
vvv <vvv@ss.com>
2020-04-05 10:38:54 +0800

    eval

--- DQN_Atari/eval.py
+++ DQN_Atari/eval.py
@@ -435,45 +435,48 @@ if training == True:
                 save_path = saver.save(sess, "./models/model-%s.ckpt" % episode)
                 print("Model Saved")
 
-with tf.Session() as sess:
-    total_test_rewards = []
-    
-    # Load the model
-    saver.restore(sess, "./models/model-23.ckpt")
-    
-    for episode in range(1):
-        total_rewards = 0
+index = 0
+while index < 200:
+    with tf.Session() as sess:
+        total_test_rewards = []
         
-        state = env.reset()
-        state, stacked_frames = stack_frames(stacked_frames, state, True)
-        
-        print("****************************************************")
-        print("EPISODE ", episode)
-        
-        while True:
-            # Reshape the state
-            state = state.reshape((1, *state_size))
-            # Get action from Q-network 
-            # Estimate the Qs values state
-            Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: state})
-            
-            # Take the biggest Q value (= the best action)
-            choice = np.argmax(Qs)
-            action = possible_actions[choice]
-            
-            #Perform the action and get the next_state, reward, and done information
-            next_state, reward, done, _ = env.step(action)
-            env.render()
-            
-            total_rewards += reward
+        # Load the model
+        saver.restore(sess, "./models/model-%s.ckpt" % index)
+        index = index + 1
 
-            if done:
-                print ("Score", total_rewards)
-                total_test_rewards.append(total_rewards)
-                break
-                
-                
-            next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
-            state = next_state
+        for episode in range(1):
+            total_rewards = 0
             
-    env.close()
+            state = env.reset()
+            state, stacked_frames = stack_frames(stacked_frames, state, True)
+            
+            print("****************************************************")
+            print("EPISODE ", episode)
+            
+            while True:
+                # Reshape the state
+                state = state.reshape((1, *state_size))
+                # Get action from Q-network 
+                # Estimate the Qs values state
+                Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: state})
+                
+                # Take the biggest Q value (= the best action)
+                choice = np.argmax(Qs)
+                action = possible_actions[choice]
+                
+                #Perform the action and get the next_state, reward, and done information
+                next_state, reward, done, _ = env.step(action)
+                env.render()
+                
+                total_rewards += reward
+
+                if done:
+                    print ("Score", total_rewards)
+                    total_test_rewards.append(total_rewards)
+                    break
+                    
+                    
+                next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+                state = next_state
+                
+        env.close()
